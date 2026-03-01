3659e07ef34fe2a6a11f3a735894965e1660b089
Kevin Konczak <konczakkevin@gmail.com>
2026-03-01 12:48:12 -0700

    index page about, experience

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     host: "127.0.0.1",
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [icon()],
 });
